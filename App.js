2413dcf007079962d44c7e83c4b24a20bd2e05c1
Harley Ferguson <askharley@outlook.com>
2021-04-22 08:43:25 +0200

    Initial commit.

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, Platform, Share } from 'react-native';
 
 export default function App() {
+  React.useEffect(() => {
+    console.log(Platform.OS);
+  }, []);
+
+  const onShare = async () => {
+    await Share.share({
+      message: 'React Native Workshop',
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity><Text style={styles.title}>The Breakdown</Text></TouchableOpacity>
+      <TextInput placeholder="Email" style={styles.input} />
+      <TextInput placeholder="Password" secureTextEntry={true} style={styles.input} />
+      <Button onPress={onShare} title="Click Me" color="#000" accessibilityLabel="Learn more" />
     </View>
   );
 }
@@ -14,8 +25,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F16281',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: '5%',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderColor: '#000',
+    width: '60%',
+    padding: '10px',
+    color: '#000',
+    marginBottom: '5%'
+  }
 });
